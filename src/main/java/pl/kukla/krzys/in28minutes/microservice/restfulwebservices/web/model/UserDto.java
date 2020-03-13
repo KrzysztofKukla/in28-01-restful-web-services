942ab551d01a4ff8860656cf561654bc1b8b6cc3
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-13 23:40:26 +0100

    37 Swagger documentation with @ApiModel

--- src/main/java/pl/kukla/krzys/in28minutes/microservice/restfulwebservices/web/model/UserDto.java
+++ src/main/java/pl/kukla/krzys/in28minutes/microservice/restfulwebservices/web/model/UserDto.java
@@ -1,6 +1,8 @@
 package pl.kukla.krzys.in28minutes.microservice.restfulwebservices.web.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,11 +21,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ApiModel(description = "UserDto description")
 public class UserDto {
 
     @Null
     private UUID id;
 
+    @ApiModelProperty(notes = "Name cannot be blank")
     @NotBlank(message = "name cannot be blank")
     private String name;
 
@@ -31,6 +35,7 @@ public class UserDto {
 //    @Null
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     @Past
+    @ApiModelProperty(notes = "Birth date must be in the past")
     private OffsetDateTime birthDate;
 
 }
