dafa89814c7c70ff583e747c9e710f4293b33a9d
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-14 15:19:11 +0100

    38 /actuator

--- src/main/java/pl/kukla/krzys/in28minutes/microservice/restfulwebservices/config/SwaggerConfig.java
+++ src/main/java/pl/kukla/krzys/in28minutes/microservice/restfulwebservices/config/SwaggerConfig.java
@@ -1,39 +1,28 @@
 package pl.kukla.krzys.in28minutes.microservice.restfulwebservices.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author Krzysztof Kukla
  */
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
 public class SwaggerConfig {
 
-    private static final Contact DEFAULT_CONTACT = new Contact("Krzysztof Kukla", "http://www", "krzys.kukla@gmail.com");
-    private static final ApiInfo DEFAULT = new ApiInfo("Api Dupa Documentation", "Api Documentation", "1.0", "urn:tos",
-        DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
-    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMERS = new HashSet<>(
-        Arrays.asList("application/json", "application/xml")
-    );
-
-    //it allows to generate swagger endpoints
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(DEFAULT)
-            .produces(DEFAULT_PRODUCES_AND_CONSUMERS)
-            .consumes(DEFAULT_PRODUCES_AND_CONSUMERS);
-    }
+//    private static final Contact DEFAULT_CONTACT = new Contact("Krzysztof Kukla", "http://www", "krzys.kukla@gmail.com");
+//    private static final ApiInfo DEFAULT = new ApiInfo("Api Dupa Documentation", "Api Documentation", "1.0", "urn:tos",
+//        DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList());
+//    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMERS = new HashSet<>(
+//        Arrays.asList("application/json", "application/xml")
+//    );
+//
+//    //it allows to generate swagger endpoints
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//            .apiInfo(DEFAULT)
+//            .produces(DEFAULT_PRODUCES_AND_CONSUMERS)
+//            .consumes(DEFAULT_PRODUCES_AND_CONSUMERS);
+//    }
 
 }
--- src/main/java/pl/kukla/krzys/in28minutes/microservice/restfulwebservices/web/model/UserDto.java
+++ src/main/java/pl/kukla/krzys/in28minutes/microservice/restfulwebservices/web/model/UserDto.java
@@ -1,8 +1,6 @@
 package pl.kukla.krzys.in28minutes.microservice.restfulwebservices.web.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,13 +19,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@ApiModel(description = "UserDto description")
+//@ApiModel(description = "UserDto description")
 public class UserDto {
 
     @Null
     private UUID id;
 
-    @ApiModelProperty(notes = "Name cannot be blank")
+    //    @ApiModelProperty(notes = "Name cannot be blank") //swagger
     @NotBlank(message = "name cannot be blank")
     private String name;
 
@@ -35,7 +33,7 @@ public class UserDto {
 //    @Null
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
     @Past
-    @ApiModelProperty(notes = "Birth date must be in the past")
+//    @ApiModelProperty(notes = "Birth date must be in the past") //swagger
     private OffsetDateTime birthDate;
 
 }
